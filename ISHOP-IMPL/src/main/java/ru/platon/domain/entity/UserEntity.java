dccc88ed2f6ae046e5402bf725742746642d99ac
Анна Елхимова <a.elkhimova@corp.nekki.ru>
2021-04-04 18:31:04 +0300

    Deleted commented code
    ISSUE-2

--- ISHOP-IMPL/src/main/java/ru/platon/domain/entity/UserEntity.java
+++ ISHOP-IMPL/src/main/java/ru/platon/domain/entity/UserEntity.java
@@ -3,7 +3,6 @@ package ru.platon.domain.entity;
 class UserEntity {
     private int id;
     private String name;
-    //private int role_id;
     private int age;
     private String phoneNumber;
     private String address;
